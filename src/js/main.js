88ed2512454e21b5809a3072e4ccb7fb2beb9a7f
Aj-Kazza <akatia@byupathway.edu>
2026-09-20 09:09:23 +1200

    add customizable product alerts

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import ProductData from './ProductData.mjs';
 import ProductList from './ProductList.mjs';
+import Alert from './Alert';
 
 const dataSource = new ProductData('tents');
 
@@ -7,3 +8,6 @@ const listElement = document.querySelector('.product-list');
 const productList = new ProductList('tents', dataSource, listElement);
 
 productList.init();
+
+const alert = new Alert();
+alert.init();
